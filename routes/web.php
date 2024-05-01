78a84624d40c2b5c7e615c59e62d324374a46dd2
nao mac <sagalumaata@gmail.com>
2024-05-02 01:45:14 +0900

    戻した

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Cache;
-
 
 Route::get('/', function () {
-	// Cache::set('test', 'yaa');
 	return view('welcome');
-});
-
-Route::get('/cache', function () {
-	// return Cache::get('test');
-});
-
-Route::get('/responce', function () {
-	return response()->json([
-		'test' => 1
-	]);
 });
